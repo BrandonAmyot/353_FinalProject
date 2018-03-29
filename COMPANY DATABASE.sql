17913bde529ff25251275a223094477923a513f5
BrandonAmyot <amyot.brandon@hotmail.com>
2018-03-28 22:22:46 -0400

    Formatting and data population
    Formatted everything and began data population for employee, dependent,
    and department

--- COMPANY DATABASE.sql
+++ COMPANY DATABASE.sql
@@ -1,56 +1,56 @@
-CREATE Database Company;
-use Company;
+CREATE DATABASE Company;
+USE Company;
 
 CREATE TABLE Employee (
-	empName		varchar(50),
-	empDOB		Date,
-	empSIN 		int KEY,
-	empAddress	varchar(50),
-	phoneNum	varchar(50)	unique,
-	salary		int,
-	empGender	char,
-	deptName	varchar(50)
+	empName		VARCHAR(50),
+	empDOB			DATE,
+	empSIN 			INT PRIMARY KEY,
+	empAddress	VARCHAR(50),
+	phoneNum		VARCHAR(12) UNIQUE,
+	salary				INT,
+	empGender		CHAR,
+	deptName		VARCHAR(50)
 );
 
 CREATE TABLE Dependent (
-	dependentName		varchar(50),
-	dependentDOB		Date,
-	dependentSIN		int KEY,
-	empSIN 				int,
-	dependentGender		char
+	dependentName		VARCHAR(50),
+	dependentDOB		DATE,
+	dependentSIN			INT PRIMARY KEY,
+	empSIN 					INT,
+	dependentGender	CHAR
 );
 
 CREATE TABLE Department (
-	deptName			varchar(50) KEY,
-	deptNumber			int unique,
-	deptManagerSIN		int unique,
-	managerStartDate	date
+	deptName				VARCHAR(50) PRIMARY KEY,
+	deptNumber			INT UNIQUE,
+	deptManagerSIN		INT UNIQUE,
+	managerStartDate	DATE
 );
 
 CREATE TABLE Projects (
-	projName		varchar(50) unique,
-    projID			int KEY,
-    projLocation 	varchar(50),
-    deptName 		varchar(50),
-    projLeaderSIN	int
+	projName		VARCHAR(50) UNIQUE,
+    projID				INT PRIMARY KEY,
+    projLocation 	VARCHAR(50),
+    deptName 		VARCHAR(50),
+    projLeaderSIN	INT
 );
 
 CREATE TABLE WorksOn (
-	empSIN		int,
-    projID		int,
-    hoursWorked	int,
-    key (empSIN, projID)
+	empSIN			INT,
+    projID				INT,
+    hoursWorked	INT,
+    PRIMARY KEY(empSIN, projID)
 );
 
 # employee getting fired, update all tables
-ALTER TABLE Department ADD CONSTRAINT fk_SIN foreign key (deptManagerSIN) references Employee (empSIN) on delete set null;
+ALTER TABLE Department ADD CONSTRAINT fk_SIN FOREIGN KEY (deptManagerSIN) REFERENCES Employee (empSIN) ON DELETE SET NULL;
 # ALTER TABLE Department ADD CONSTRAINT fk_SIN foreign key (managerStartDate) references Employee (empSIN) on delete set null;
-ALTER TABLE Dependent ADD CONSTRAINT fk_dependent foreign key (empSIN) references Employee (empSIN) on delete cascade;
-ALTER TABLE Projects ADD CONSTRAINT fk_projSIN foreign key (projLeaderSIN) references Employee (empSIN) on delete set null;
+ALTER TABLE Dependent ADD CONSTRAINT fk_dependent FOREIGN KEY (empSIN) REFERENCES Employee (empSIN) ON DELETE CASCADE;
+ALTER TABLE Projects ADD CONSTRAINT fk_projSIN FOREIGN KEY (projLeaderSIN) REFERENCES Employee (empSIN) ON DELETE SET NULL;
 
 # Department name is changed or closed down, update all tables
-ALTER TABLE projects ADD CONSTRAINT fk_projDeptName FOREIGN KEY (deptName) references department (deptName) on delete set null on update cascade;
-ALTER TABLE employee ADD CONSTRAINT fk_empDeptName FOREIGN KEY (deptName) references department (deptName) on delete set null on update cascade;
+ALTER TABLE projects ADD CONSTRAINT fk_projDeptName FOREIGN KEY (deptName) REFERENCES department (deptName) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE employee ADD CONSTRAINT fk_empDeptName FOREIGN KEY (deptName) REFERENCES department (deptName) ON DELETE SET NULL ON UPDATE CASCADE;
 
 
 #---------------------------------------------------------------------------------------------------------------------
---
+++ Employee_population.sql
@@ -0,0 +1,32 @@
+USE company;
+
+# Sales
+INSERT INTO employee VALUES ('Michael Scott', DATE '15-03-1964', 0001, '12 Oak St', '514-111-1243', 58000, 'M', 'Sales');
+INSERT INTO employee VALUES ('Dwight Schrute', DATE '20-01-1977', 0002, '27 Elm St', '514-376-1243', 52000, 'M', 'Sales');
+INSERT INTO employee VALUES ('Jim Halpert', DATE '01-10-1978', 0003, '54 Steam St', '514-172-4321', 50000, 'M', 'Sales');
+INSERT INTO employee VALUES ('Stanley Hudson', DATE '19-02-1958', 0004, '421 Willow St', '514-329-1000', 51000, 'M', 'Sales');
+INSERT INTO employee VALUES ('Phyllis Vance', DATE '10-07-1964', 0005, '790 Maple St', '514-300-2741', 50000, 'F', 'Sales');
+INSERT INTO employee VALUES ('Andrew Bernard', DATE '24-01-1973', 0006, '47 Ash St', '514-271-3810', 48000, 'M', 'Sales');
+
+#Accounting
+INSERT INTO employee VALUES ('Angela Schrute', DATE '25-06-1971', 0007, '27 Elm St', '514-378-1432', 46000, 'F', 'Accounting');
+INSERT INTO employee VALUES ('Kevin Malone', DATE '01-06-1968', 0008, '24 Stanford Ave', '514-390-3478', 43000, 'M', 'Accounting');
+INSERT INTO employee VALUES ('Oscar Martinez', DATE '18-11-1958', 0009, '33 Fir St', '514-128-9039', 43000, 'M', 'Accounting');
+
+#Quality Assurance
+INSERT INTO employee VALUES ('Creed Bratton', DATE '08-02-1943', 0010, '75 Staple Ave', '514-222-1420', 42000, 'M', 'Quality Assurance');
+INSERT INTO employee VALUES ('Meredith Palmer', DATE '12-05-1959', 0011, '33 Charleston St', '514-333-3857', 42000, 'F', 'Quality Assurance');
+
+#Customer Service
+INSERT INTO employee VALUES ('Kelly Kapoor', DATE '05-02-1980', 0012, '72 Prince Ave', '514-123-0947', 44000, 'F', 'Customer Service');
+INSERT INTO employee VALUES ('Ryan Howard', DATE '05-05-1979', 0013, '46 King Crescent', '514-957-3840', 46000, 'M', 'Customer Service');
+
+#Human Resources
+INSERT INTO employee VALUES ('Toby Flenderson', DATE '22-02-1971', 0014, '957 Park Ave', '514-119-6778', 48000, 'M', 'Human Resources');
+INSERT INTO employee VALUES ('Holly Flax', DATE '18-11-1958', 0015, '12 Oak St', '514-128-9039', 47500, 'F', 'Human Resources');
+INSERT INTO employee VALUES ('Pamela Halpert', DATE '25-03-1979', 0016, '54 Steam St', 42500, 'F', 'Human Resources');
+
+#Shipping
+INSERT INTO employee VALUES ('Darryl Philbin', DATE '25-10-1971', 0017, '12 Jackson St', '514-987-3739', 52000, 'M', 'Shipping');
+INSERT INTO employee VALUES ('Roy Anderson', DATE '25-07-1973', 0018, '154 Park Ave', '514-205-2820', 45000, 'M', 'Shipping');
+INSERT INTO employee VALUES ('Madge Madsen', DATE '29-10-1962', 0019, '743 Ford St', '514-297-1205', 43000, 'F', 'Shipping');
