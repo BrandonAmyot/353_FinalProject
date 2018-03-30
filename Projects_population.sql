1a7a4be1503d534ea558de22b08503e88b4ee3e6
MarkAndersonTrocme <mark.anderson.trocme@gmail.com>
2018-03-30 19:47:31 -0400

    Added Projects

---
+++ Projects_population.sql
@@ -0,0 +1,11 @@
+USE Company;
+
+INSERT INTO projects VALUES ('salesForce', 100, 'montreal', 'Sales', 0002);
+INSERT INTO projects VALUES ('MoneyProject', 101, 'toronto', 'Accounting', 0008);
+INSERT INTO projects VALUES ('GoodEnough', 102, 'montreal', 'Quality Assurance', 0011);
+INSERT INTO projects VALUES ('CustomerIsRight', 103, 'calgary', 'Customer Service', 0013);
+INSERT INTO projects VALUES ('HelpMe', 104, 'quebec', 'Human Resources', 0015);
+INSERT INTO projects VALUES ('Gone', 105, 'ottawa', 'Shipping', 0018);
+INSERT INTO projects VALUES ('SellSell', 106, 'montreal', 'Sales', 0009);
+INSERT INTO projects VALUES ('FedEx', 107, 'saskatoon', 'Shipping', 0019);
+INSERT INTO projects VALUES ('TopNotch', 108, 'new york', 'Quality Assurance', 0009);
