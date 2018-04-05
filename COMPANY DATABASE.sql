a6445214eb0e8022c954254331da148357a55b6a
BrandonAmyot <amyot.brandon@hotmail.com>
2018-04-05 16:18:38 -0400

    modified schemas
    Added hourly salary, constraints, changed foreign keys to its own file.

--- COMPANY DATABASE.sql
+++ COMPANY DATABASE.sql
@@ -2,26 +2,28 @@ CREATE DATABASE Company;
 USE Company;
 
 CREATE TABLE Employee (
-	empName		VARCHAR(50),
+	empName		VARCHAR(30),
 	empDOB			DATE,
 	empSIN 			INT PRIMARY KEY,
 	empAddress	VARCHAR(50),
 	phoneNum		VARCHAR(12) UNIQUE,
-	salary				INT,
+	hourlySalary	INT,
 	empGender		CHAR,
-	deptName		VARCHAR(50)
+	deptName		VARCHAR(30),
+    CONSTRAINT chk_gender CHECK (empGender IN ('M', 'F'))
 );
 
 CREATE TABLE Dependent (
-	dependentName		VARCHAR(50),
+	dependentName		VARCHAR(30),
 	dependentDOB		DATE,
 	dependentSIN			INT PRIMARY KEY,
 	empSIN 					INT,
-	dependentGender	CHAR
+	dependentGender	CHAR,
+    CONSTRAINT chk_gender CHECK (dependentGender IN ('M', 'F'))
 );
 
 CREATE TABLE Department (
-	deptName				VARCHAR(50) PRIMARY KEY,
+	deptName				VARCHAR(30) PRIMARY KEY,
 	deptNumber			INT UNIQUE,
 	deptManagerSIN		INT UNIQUE,
 	managerStartDate	DATE
@@ -30,9 +32,11 @@ CREATE TABLE Department (
 CREATE TABLE Projects (
 	projName		VARCHAR(50) UNIQUE,
     projID				INT PRIMARY KEY,
-    projLocation 	VARCHAR(50),
-    deptName 		VARCHAR(50),
-    projLeaderSIN	INT
+    projLocation 	VARCHAR(30),
+    projPhase		VARCHAR(50),
+    deptName 		VARCHAR(30),
+    projLeaderSIN	INT,
+    CONSTRAINT chk_phase CHECK (projPhase IN ('preliminary', 'intermediate', 'advanced', 'complete'))
 );
 
 CREATE TABLE WorksOn (
@@ -41,22 +45,3 @@ CREATE TABLE WorksOn (
     hoursWorked	INT,
     PRIMARY KEY(empSIN, projID)
 );
-
-# employee getting fired, update all tables
-ALTER TABLE Department ADD CONSTRAINT fk_SIN FOREIGN KEY (deptManagerSIN) REFERENCES Employee (empSIN) ON DELETE SET NULL;
-# ALTER TABLE Department ADD CONSTRAINT fk_SIN foreign key (managerStartDate) references Employee (empSIN) on delete set null;
-ALTER TABLE Dependent ADD CONSTRAINT fk_dependent FOREIGN KEY (empSIN) REFERENCES Employee (empSIN) ON DELETE CASCADE;
-ALTER TABLE Projects ADD CONSTRAINT fk_projSIN FOREIGN KEY (projLeaderSIN) REFERENCES Employee (empSIN) ON DELETE SET NULL;
-
-# Department name is changed or closed down, update all tables
-ALTER TABLE projects ADD CONSTRAINT fk_projDeptName FOREIGN KEY (deptName) REFERENCES department (deptName) ON DELETE SET NULL ON UPDATE CASCADE;
-ALTER TABLE employee ADD CONSTRAINT fk_empDeptName FOREIGN KEY (deptName) REFERENCES department (deptName) ON DELETE SET NULL ON UPDATE CASCADE;
-
-
-#---------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
--- Projects_population.sql
+++ Projects_population.sql
@@ -1,11 +1,13 @@
 USE Company;
 
-INSERT INTO projects VALUES ('salesForce', 100, 'montreal', 'Sales', 0002);
-INSERT INTO projects VALUES ('MoneyProject', 101, 'toronto', 'Accounting', 0008);
-INSERT INTO projects VALUES ('GoodEnough', 102, 'montreal', 'Quality Assurance', 0010);
-INSERT INTO projects VALUES ('CustomerIsRight', 103, 'calgary', 'Customer Service', 0013);
-INSERT INTO projects VALUES ('HelpMe', 104, 'quebec', 'Human Resources', 0015);
-INSERT INTO projects VALUES ('Gone', 105, 'ottawa', 'Shipping', 0018);
-INSERT INTO projects VALUES ('SellSell', 106, 'montreal', 'Sales', 0006);
-INSERT INTO projects VALUES ('FedEx', 107, 'saskatoon', 'Shipping', 0019);
-INSERT INTO projects VALUES ('TopNotch', 108, 'new york', 'Quality Assurance', 0011);
+INSERT INTO projects VALUES ('salesForce', 100, 'montreal', 'advanced', 'Sales', 0002);
+INSERT INTO projects VALUES ('MoneyProject', 101, 'toronto', 'complete', 'Accounting', 0008);
+INSERT INTO projects VALUES ('GoodEnough', 102, 'montreal', 'preliminary', 'Quality Assurance', 0010);
+INSERT INTO projects VALUES ('CustomerIsRight', 103, 'calgary', 'advanced', 'Customer Service', 0013);
+INSERT INTO projects VALUES ('HelpMe', 104, 'quebec', 'intermediate', 'Human Resources', 0015);
+INSERT INTO projects VALUES ('Gone', 105, 'ottawa', 'complete', 'Shipping', 0018);
+INSERT INTO projects VALUES ('SellSell', 106, 'montreal', 'preliminary', 'Sales', 0006);
+INSERT INTO projects VALUES ('FedEx', 107, 'saskatoon', 'advanced', 'Shipping', 0019);
+INSERT INTO projects VALUES ('TopNotch', 108, 'new york', 'intermediate', 'Quality Assurance', 0011);
+
+INSERT INTO projects VALUES ('Bottomnotch', 109, 'new york', 'beginning', 'Quality Assurance', 0011);
